6814f614a859f19bd1ec60c9ed248d5844cdd906
stroembulator <namatad@gmx.de>
2016-01-05 10:11:46 +0100

    add scan/detail dialog experimental

--- src/main/java/de/exaxway/cleanstore/PhotoCamView.java
+++ src/main/java/de/exaxway/cleanstore/PhotoCamView.java
@@ -1,20 +1,34 @@
 package de.exaxway.cleanstore;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpSession;
 
 import org.primefaces.event.CaptureEvent;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 @ManagedBean
 @ViewScoped
@@ -43,6 +57,8 @@ public class PhotoCamView {
 	private DefaultStreamedContent xx;
 	private DefaultStreamedContent lastCaptured = null;
 	private String lastCapturedName = null;
+
+	private String code = null;
 
 	public StreamedContent getImage() {
 		return xx;
@@ -76,5 +92,34 @@ public class PhotoCamView {
 
 	public String getLastCapturedName() {
 		return lastCapturedName;
+	}
+	
+	public void oncaptureCode(final CaptureEvent captureEvent) {
+		LOG.info("oncaptureCode");
+		byte[] data = captureEvent.getData();
+		try {
+			code = readQRCode(data);
+		} catch (Exception e) {
+			LOG.log(Level.WARNING, "scan failed", e);
+		}
+		
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public static String readQRCode(byte[] data)
+			throws FileNotFoundException, IOException, NotFoundException {
+			Map hintMap = new HashMap();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+	
+		
+		BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
+				new BufferedImageLuminanceSource(
+						ImageIO.read(new ByteArrayInputStream(data)))));
+		Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap,
+				hintMap);
+		return qrCodeResult.getText();
 	}
 }
